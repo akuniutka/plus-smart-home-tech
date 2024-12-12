a4da45a2363a8cbeb84586713b9fd03a5a66e23b
Andrei Kuniutka <akuniutka@gmail.com>
2024-12-12 22:10:37 +0300

    Fix: Rewrite Collector with Strategy pattern

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/contoller/HubEventController.java
@@ -0,0 +1,48 @@
+package ru.yandex.practicum.kafka.telemetry.contoller;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.kafka.telemetry.dto.hub.HubEvent;
+import ru.yandex.practicum.kafka.telemetry.dto.hub.HubEventType;
+import ru.yandex.practicum.kafka.telemetry.service.handler.HubEventHandler;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/events/hubs")
+@Slf4j
+public class HubEventController {
+
+    private final Map<HubEventType, HubEventHandler> hubEventHandlers;
+    private final Clock clock;
+
+    public HubEventController(final Set<HubEventHandler> hubEventHandlers, final Clock clock) {
+        this.hubEventHandlers = hubEventHandlers.stream()
+                .collect(Collectors.toMap(HubEventHandler::getMessageType, Function.identity()));
+        this.clock = clock;
+    }
+
+    @PostMapping
+    public void collect(@Valid @RequestBody HubEvent event) {
+        log.debug("Received request with hub event: {}", event);
+        if (event.getTimestamp() == null) {
+            event.setTimestamp(Instant.now(clock));
+        }
+        if (hubEventHandlers.containsKey(event.getType())) {
+            hubEventHandlers.get(event.getType()).handle(event);
+        } else {
+            throw new IllegalArgumentException("No handler found for hub event type: " + event.getType());
+        }
+        log.debug("Responded 200 OK to hub event request");
+    }
+
+}
--- telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/util/KafkaSender.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/util/KafkaSender.java
@@ -2,7 +2,9 @@ package ru.yandex.practicum.kafka.telemetry.util;
 
 import org.apache.avro.specific.SpecificRecordBase;
 
+import java.time.Instant;
+
 public interface KafkaSender {
 
-    void send(String topic, String Key, Long timestamp, SpecificRecordBase message);
+    void send(String topic, String key, Instant timestamp, SpecificRecordBase message);
 }
