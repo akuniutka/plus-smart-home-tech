62a1f4c73b3fbffced605344e460c079a5ffe770
Andrei Kuniutka <akuniutka@gmail.com>
2024-12-25 17:51:46 +0300

    Fix: Move Kafka listener thread management to KafkaListener class

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/configuration/CollectorConfiguration.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/configuration/CollectorConfiguration.java
@@ -2,15 +2,13 @@ package ru.yandex.practicum.telemetry.collector.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import ru.yandex.practicum.kafka.telemetry.client.KafkaSender;
 import ru.yandex.practicum.kafka.telemetry.client.KafkaSenderConfig;
 
 @Configuration
-public class KafkaClientConfiguration {
+public class CollectorConfiguration {
 
     @Bean(destroyMethod = "close")
-    @Lazy
     public KafkaSender kafkaSender(final KafkaSenderConfig config) {
         return new KafkaSender(config);
     }
