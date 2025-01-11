0b862e395deb252c597be7df2a66e2016ac6a02a
Andrei Kuniutka <akuniutka@gmail.com>
2025-01-11 19:31:09 +0300

    Fix: Fix shutting Kafka down in Aggregator integration test

--- telemetry/aggregator/src/test/java/ru/yandex/practicum/telemetry/aggregator/AggregatorAppIT.java
+++ telemetry/aggregator/src/test/java/ru/yandex/practicum/telemetry/aggregator/AggregatorAppIT.java
@@ -1,6 +1,8 @@
 package ru.yandex.practicum.telemetry.aggregator;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -8,13 +10,32 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
+import ru.yandex.practicum.kafka.telemetry.client.KafkaListener;
+import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 
 @SpringBootTest
 @Testcontainers
 class AggregatorAppIT {
 
+    // Reference to KafkaListener bean to stop it after JUnit runs all the tests. JUnit shuts
+    // down Kafka Testcontainers before Spring destroys application context. Between these two
+    // events the listener will signal "Group coordinator is unavailable" if keeps running.
+    private static KafkaListener<SensorEventAvro> sensorEventListener;
+
     @Container
     static final KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("apache/kafka:3.7.2"));
+
+    @Autowired
+    public AggregatorAppIT(final KafkaListener<SensorEventAvro> sensorEventListener) {
+        AggregatorAppIT.sensorEventListener = sensorEventListener;
+    }
+
+    @AfterAll
+    static void destroy() {
+        if (sensorEventListener != null) {
+            sensorEventListener.stop();
+        }
+    }
 
     @DynamicPropertySource
     static void overrideProperties(final DynamicPropertyRegistry registry) {
